e7fa51145b4f0e027c251305c59a7f77955731a3
Jamie Blondin <jblondin@gmail.com>
2017-05-06 12:05:41 -0400

    Initial commit with floating point assertions

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+
+#[macro_export]
+macro_rules! assert_fp_eq {
+    ($lhs:expr, $rhs:expr, $eps:expr) => {{
+        assert!((($lhs - $rhs) as f64).abs() < $eps);
+    }};
+    ($lhs:expr, $rhs:expr) => {{
+        const DEFAULT_EPS: f64 = 1.0e-6;
+        assert_fp_eq!($lhs, $rhs, DEFAULT_EPS);
+    }};
+}
+#[macro_export]
+macro_rules! assert_fpvec_eq {
+    ($lhs:expr, $rhs:expr, $eps:expr) => {{
+        use std::f64;
+        assert!(($lhs.iter().zip($rhs.iter()))
+            .fold(f64::NEG_INFINITY, |acc, (&l, &r)| acc.max((l as f64 - r as f64).abs())) < $eps);
+    }};
+    ($lhs:expr, $rhs:expr) => {{
+        const DEFAULT_EPS: f64 = 1.0e-6;
+        assert_fpvec_eq!($lhs, $rhs, DEFAULT_EPS);
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_assert_fp_eq() {
+        assert_fp_eq!(5.1000001, 5.1000002);
+        assert_fp_eq!(5.1000001, 5.1000002, 1e-4);
+    }
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_assert_fp_eq_fail() {
+        assert_fp_eq!(5.1000001, 5.1000002, 1.0e-8);
+    }
+
+    #[test]
+    fn test_assert_fpvec_eq() {
+        assert_fpvec_eq!([5.1000001, 6.2000002], [5.1000002, 6.2000005]);
+        assert_fpvec_eq!([5.1000001, 6.2000002], [5.1000002, 6.2000005], 1e-4);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_assert_fpvec_eq_fail() {
+        assert_fpvec_eq!([5.100000001, 6.200000002], [5.1000002, 6.2000005], 1e-8);
+    }
+}
